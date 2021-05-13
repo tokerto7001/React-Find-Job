cc35725a9b2ffd10697c0dc874a37eae99d9ad7e
Hasan TOKER <75892548+tokerto7001@users.noreply.github.com>
2021-05-13 18:24:06 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
 import {useState, useEffect} from "react"
-import cat from "./octocat.png"
-import job from "./job-logo.svg"
-import design from "./design.svg"
+import cat from "./images/octocat.png"
+import job from "./images/job-logo.svg"
+import design from "./images/design.svg"
+import loadingLogo from "./images/loading.gif"
+import error from "./images/404.png"
+import axios from "axios"
+import  Card  from './components/Card';
+
 
 function App() {
 
   const [ description, setDescription ] = useState("")
   const [ location, setLocation ] = useState("")
+  const [ loading, setLoading ] = useState(false)
+  const [ jobList, setJobList ] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchData()
+    setLocation("")
+    setDescription("")
+  }
+
+  const fetchData = () => {
+    setLoading(true)
+    axios.get( `/positions.json?description=${description}&location=${location}` )
+    .then((res) => {
+      setJobList(res.data)
+      console.log(res.data)
+    })
+    .then(() => setLoading(false) )
+    
+  }
+
+  useEffect(() => {
+   fetchData()
+  }, [])
 
   return (
     <div className="App">
@@ -17,14 +46,29 @@ function App() {
       </div>
       <h1 className="title" >GITHUB JOB FINDER</h1>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="DESCRIPTION" value={description} onChange={(e) => setDescription(e.target.value)} /> <br />
           <input type="text" placeholder="LOCATION" value={location} onChange={(e) => setLocation(e.target.value)} /> <br />
           <button className="search">SEARCH</button>
         </form>
       </div>
+      {
+        loading ?  <img  src={loadingLogo} /> : jobList.length < 1 ? <img className="error" src={error} /> :
+    
+      jobList?.map((job) => (
+        <Card 
+        img={job.company_logo}
+        title={job.title}
+        name={job.company}
+        location={job.location}
+        type={job.type}
+        url={job.company_url}
+        />
+      ))
+    }
+    
     <div className="footer">
-    <h1>{"<hasan7001 />"}</h1>
+    <h1>{"<tokerto7001 />"}</h1>
     <img src={design} className="design" />
     <h1>DESIGN</h1>
     </div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Card = ({
+    img, title, name, location, type, url
+}) => {
+
+    return(
+
+        <div className="card">
+            <div className="logo-div">
+                <img className="company-logo" src={img} />
+            </div>
+            <div className="card-body">
+                <div className="body-title">
+                    {title}
+                </div>
+                <div className="body-desc">
+                    {name} - {location}
+                </div>
+                <div className="body-type">
+                    {type}
+                </div>
+                <a href={url} target="_blank"><button className="apply">APPLY</button></a>
+            </div>
+        </div>
+    )
+
+}
+export default Card;
